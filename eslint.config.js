6aaef1dbc3176fbc5bac7a4150669b9b862ffb1e
Nadiia Bryk <25nadin47@gmail.com>
2025-07-05 10:07:13 +0300

    setup with ESLint, Prettier, Husky

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-//import { globalIgnores } from 'eslint/config'
 
 import prettier from 'eslint-config-prettier';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
@@ -11,29 +10,18 @@ import importPlugin from 'eslint-plugin-import';
 import react from 'eslint-plugin-react';
 
 export default tseslint.config([
-  // globalIgnores(['dist']),
   { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      //reactHooks.configs['recommended-latest'],
-      //reactRefresh.configs.vite,
-      //react.configs.recommended,
-      // "plugin:react/recommended",
-      //'plugin:jsx-a11y/recommended',
-      //'plugin:import/recommended',
-      //importPlugin.configs.recommended,
       prettier,
     ],
     plugins: {
       'react-hooks': reactHooks,
-      //reactHooks,
       'react-refresh': reactRefresh,
       react,
-      //'jsx-a11y': jsxA11y,
-      //'import': importPlugin,
     },
     languageOptions: {
       ecmaVersion: 2020,
@@ -53,8 +41,6 @@ export default tseslint.config([
         { allowConstantExport: true },
       ],
       // Ваші власні, додаткові правила або перевизначення
-      //'jsx-a11y/alt-text': 'warn',
-      //'jsx-a11y/anchor-is-valid': 'warn',
     },
     settings: {
       react: {
