7f14cac044258968340f696a77b45831c3666f27
TetianaHodis1308 <hodistetianajob@ukr.net>
2025-07-08 17:14:40 +0300

    added product sliders

--- src/components/ProductsSlider/ProductsSlider.tsx
+++ src/components/ProductsSlider/ProductsSlider.tsx
@@ -1,25 +1,77 @@
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Navigation } from 'swiper/modules';
-// // import 'swiper/css';
-// // import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import { ArrowLeftButton } from '../../images/icons/ArrowLeftButton';
+import { ArrowRightButton } from '../../images/icons/ArrowRightButton';
+import { ProductCard } from '../ProductCard';
 
-// export const ProductSlider = () => {
-//   return (
-//     <Swiper
-//       modules={[Navigation]}      // Підключаємо модуль навігації
-//       navigation                  // Вмикаємо стрілки
-//       spaceBetween={16}
-//       slidesPerView={4}
-//     >
-//       <SwiperSlide className="bg-red-500">Slide 1</SwiperSlide>
-//       <SwiperSlide className="bg-red-500">Slide 2</SwiperSlide>
-//       <SwiperSlide className="bg-red-500">Slide 3</SwiperSlide>
-//       <SwiperSlide className="bg-red-500">Slide 4</SwiperSlide>
-//       <SwiperSlide className="bg-red-500">Slide 1www</SwiperSlide>
-//       <SwiperSlide className="bg-red-500">Slide 2www</SwiperSlide>
-//       <SwiperSlide className="bg-red-500">Slide www</SwiperSlide>
-//       <SwiperSlide className="bg-red-500">Slide 4www</SwiperSlide>
-//       {/* ...інші слайди */}
-//     </Swiper>
-//   );
-// };
+export type ProductSliderProps = {
+  sliderConfig: {
+    titleForBrand: string;
+    classNameForButtonPrev: string;
+    classNameForButtonNext: string;
+  };
+};
+
+export const ProductSlider = ({ sliderConfig }: ProductSliderProps) => {
+  const { titleForBrand, classNameForButtonPrev, classNameForButtonNext } =
+    sliderConfig;
+
+  return (
+    <>
+      <div className="relative mb-5">
+        <h2 className="h2">{titleForBrand}</h2>
+        <div className="absolute right-0 top-1 flex gap-4">
+          <button className={`${classNameForButtonPrev} group cursor-pointer`}>
+            <ArrowLeftButton />
+          </button>
+          <button className={`${classNameForButtonNext} group cursor-pointer`}>
+            <ArrowRightButton />
+          </button>
+        </div>
+      </div>
+
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          prevEl: `.${classNameForButtonPrev}`,
+          nextEl: `.${classNameForButtonNext}`,
+        }}
+        loop={true}
+        spaceBetween={16}
+        breakpoints={{
+          320: {
+            slidesPerView: 1.2,
+          },
+          640: {
+            slidesPerView: 2.2,
+          },
+          1200: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProductCard />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
