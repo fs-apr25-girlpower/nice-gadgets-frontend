be1ca3a6448a719a1aa315526df9cec1f9d3e4d0
TetianaHodis1308 <hodistetianajob@ukr.net>
2025-07-10 12:40:52 +0300

    added correct data in ShopByCategory

--- src/components/ShopByCategory/ShopByCategory.tsx
+++ src/components/ShopByCategory/ShopByCategory.tsx
@@ -1,9 +1,16 @@
 import { NavLink } from 'react-router-dom';
-import phone from '/img/categories/category-phones.webp';
-import tablet from '/img/categories/category-tablets.webp';
-import phones from '/img/categories/category-accessories.webp';
+import phoneImge from '/img/categories/category-phones.webp';
+import tabletImage from '/img/categories/category-tablets.webp';
+import phonesImage from '/img/categories/category-accessories.webp';
+import { usePhones } from '../../context/PhonesContext';
+import { useTablets } from '../../context/TabletsContext';
+import { useAccessories } from '../../context/AccessoriesContext';
 
 export const ShopByCategory = () => {
+  const phonesLength = usePhones().length;
+  const tabletsLength = useTablets().length;
+  const accessoriesLength = useAccessories().length;
+
   return (
     <section className="mt-14 tablet:mt-16 desktop:mt-20">
       <div className="flex flex-col">
@@ -16,7 +23,7 @@ export const ShopByCategory = () => {
           >
             <div className="w-full aspect-square mb-4 transition-transform duration-300 hover:scale-110">
               <img
-                src={phone}
+                src={phoneImge}
                 alt="Phone"
                 className="w-full h-full object-cover"
               />
@@ -24,9 +31,7 @@ export const ShopByCategory = () => {
             <h4 className="h4 color-primary mb-1 hover:underline transition-all duration-200">
               Mobile phones
             </h4>
-            <p className="color-secondary mb-6">
-              95 models{/* mobile phones .lenght */}
-            </p>
+            <p className="color-secondary mb-6">{phonesLength} models</p>
           </NavLink>
 
           <NavLink
@@ -35,7 +40,7 @@ export const ShopByCategory = () => {
           >
             <div className="w-full aspect-square mb-4 transition-transform duration-300 hover:scale-110">
               <img
-                src={tablet}
+                src={tabletImage}
                 alt="Tablets"
                 className="w-full h-full object-cover"
               />
@@ -43,9 +48,7 @@ export const ShopByCategory = () => {
             <h4 className="h4 color-primary mb-1 hover:underline transition-all duration-200">
               Tablets
             </h4>
-            <p className="color-secondary mb-6">
-              95 models{/* Tablets .lenght */}
-            </p>
+            <p className="color-secondary mb-6">{tabletsLength} models</p>
           </NavLink>
 
           <NavLink
@@ -54,7 +57,7 @@ export const ShopByCategory = () => {
           >
             <div className="w-full aspect-square mb-4 transition-transform duration-300 hover:scale-110">
               <img
-                src={phones}
+                src={phonesImage}
                 alt="Phone"
                 className="w-full h-full object-cover"
               />
@@ -62,9 +65,7 @@ export const ShopByCategory = () => {
             <h4 className="h4 color-primary mb-1 hover:underline transition-all duration-200">
               Accessories
             </h4>
-            <p className="color-secondary">
-              95 models{/* mobile phones .lenght */}
-            </p>
+            <p className="color-secondary">{accessoriesLength} models</p>
           </NavLink>
         </div>
       </div>
